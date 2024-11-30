ab5c4bc2129f227424df8d99c77b578ecabeedc2
Nicolas Lussoro <lussoro8@gmail.com>
2024-11-30 19:12:21 -0300

    import faltantes from 'sprint_8_api/Cliente/serializer.py';

--- sprint_8_api/Cliente/serializer.py
+++ sprint_8_api/Cliente/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Customer
+from .models import Customer, CustomerAccount, Loan, AccountType, Card, CardType
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
